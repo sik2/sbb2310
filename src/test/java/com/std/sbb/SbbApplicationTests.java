d9e0a2b01107c6c129c653a052cea37b3e723ff6
sik2 <bickwang123@gmail.com>
2023-11-16 10:42:57 +0900

    리포지터리 - 답변 조회하기

--- src/test/java/com/std/sbb/SbbApplicationTests.java
+++ src/test/java/com/std/sbb/SbbApplicationTests.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -75,4 +77,17 @@ class SbbApplicationTests {
 		assertEquals(2, a.getQuestion().getId());
 	}
 
+	@Test
+	@Transactional
+	@DisplayName("질문 기준 답변가져오기")
+	void test011() {
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+
+		List<Answer> answerList = q.getAnswerList();
+
+		assertEquals(1, answerList.size());
+		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+	}
 }
