d0d5ed390834b2a3331b1efae5e475ee0db8639c
sik2 <bickwang123@gmail.com>
2023-11-17 11:51:43 +0900

    QuestionService 서비스 도입

--- src/main/java/com/std/sbb/Question/QuestionRepository.java
+++ src/main/java/com/std/sbb/Question/QuestionRepository.java
@@ -1,7 +1,9 @@
 package com.std.sbb.Question;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
 }
