3c6cadc4fb9d9194db86b246b7d6c3bae8c58805
sik2 <bickwang123@gmail.com>
2023-11-17 13:34:58 +0900

    답변 등록 - 답변 등록 버튼 만들기

--- src/main/java/com/std/sbb/Question/QuestionController.java
+++ src/main/java/com/std/sbb/Question/QuestionController.java
@@ -3,9 +3,7 @@ package com.std.sbb.Question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,5 +28,12 @@ public class QuestionController {
         model.addAttribute("question", q);
 
         return "question_detail";
+    }
+
+    //TODO: 테스트용 AnswerController 에서 처리 예정
+    @PostMapping("/createTemp")
+    @ResponseBody
+    public String temp() {
+        return "전송완료";
     }
 }
