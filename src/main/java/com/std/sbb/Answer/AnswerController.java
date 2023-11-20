290ef8d0d4ee36740ae64443aa4991bdeb3a008c
sik2 <bickwang123@gmail.com>
2023-11-20 10:23:00 +0900

    답변저장

--- src/main/java/com/std/sbb/Answer/AnswerController.java
+++ src/main/java/com/std/sbb/Answer/AnswerController.java
@@ -5,23 +5,21 @@ import com.std.sbb.Question.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/answer")
 @RequiredArgsConstructor
 public class AnswerController {
     private final QuestionService questionService;
+    private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
     public String createAnswer (Model model, @PathVariable("id") Integer id, @RequestParam String content) {
         //답변 부모 질문객체를 받아온다.
         Question q = this.questionService.getQuestion(id);
 
-        // TODO : 답변객체를 만든다.
+        Answer answer = this.answerService.create(q, content);
 
         return "redirect:/question/detail/%d".formatted(id);
     }
