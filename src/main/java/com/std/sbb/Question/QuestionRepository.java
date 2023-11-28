6a87b29887535b70bd57a24393cb133133b16b5b
sik2 <bickwang123@gmail.com>
2023-11-28 12:16:22 +0900

    , 페이징 - 페이징 구현하기

--- src/main/java/com/std/sbb/Question/QuestionRepository.java
+++ src/main/java/com/std/sbb/Question/QuestionRepository.java
@@ -1,9 +1,12 @@
 package com.std.sbb.Question;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
+    Page<Question> findAll(Pageable pageable);
 }
--- src/main/java/com/std/sbb/Question/QuestionService.java
+++ src/main/java/com/std/sbb/Question/QuestionService.java
@@ -2,6 +2,9 @@ package com.std.sbb.Question;
 
 import com.std.sbb.DataNotException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -34,5 +37,10 @@ public class QuestionService {
 
             this.questionRepository.save(q);
             return q;
+    }
+
+    public Page<Question> getList(int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+        return this.questionRepository.findAll(pageable);
     }
 }
