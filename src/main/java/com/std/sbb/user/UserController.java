a327178ae7720126aa8addcf2ee5ff6ae965e7b6
sik2 <bickwang123@gmail.com>
2023-11-29 09:42:24 +0900

    회원가입네비 예ㅚ

--- src/main/java/com/std/sbb/user/UserController.java
+++ src/main/java/com/std/sbb/user/UserController.java
@@ -2,6 +2,7 @@ package com.std.sbb.user;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,8 +33,20 @@ public class UserController {
             return "signup_form";
         }
 
-        this.userService.create(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword1());
+        try {
+            this.userService.create(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword1());
+
+        } catch (DataIntegrityViolationException e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+            return "signup_form";
+        } catch (Exception e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "signup_form";
+        }
 
         return "redirect:/";
     }
+
 }
