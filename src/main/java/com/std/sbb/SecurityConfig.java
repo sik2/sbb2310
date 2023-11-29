76c76384dd41976a7d65c8dc8287a0626d653abf
sik2 <bickwang123@gmail.com>
2023-11-29 10:36:10 +0900

    UserDetailsService를 구현

--- src/main/java/com/std/sbb/SecurityConfig.java
+++ src/main/java/com/std/sbb/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.std.sbb;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,6 +18,9 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
                         .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+                        .formLogin((formLogin) -> formLogin
+                                .loginPage("/user/login")
+                                .defaultSuccessUrl("/"))
         ;
         return http.build();
     }
@@ -23,5 +28,10 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
